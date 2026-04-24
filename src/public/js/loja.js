1f2aeb171c2b3f62f41bd9ef96cce2f67bbd3feb
Cleiton Da Silva Batista <cleitondasilvabatista11@gmail.com>
2026-04-24 08:49:11 -0300

    Corrigindo modal display block

--- src/public/js/loja.js
+++ src/public/js/loja.js
@@ -179,9 +179,34 @@ function ativarBotoesCarrinho() {
 }
 
 async function adicionarAoCarrinho(idItem) {
-    if (!exigirLoginParaCarrinho()) {
-    return;
-    }
+    function exigirLoginParaCarrinho() {
+  const token = localStorage.getItem('jobee_token');
+
+  if (token) return true;
+
+  const modal = document.getElementById('modal-login');
+  modal.classList.add('active'); // 👈 mostra modal
+
+  return false;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const btnLogin = document.getElementById('btn-login-agora');
+  const btnDepois = document.getElementById('btn-login-depois');
+  const modal = document.getElementById('modal-login');
+
+  if (btnLogin) {
+    btnLogin.addEventListener('click', () => {
+      window.location.href = '/login';
+    });
+  }
+
+  if (btnDepois) {
+    btnDepois.addEventListener('click', () => {
+      modal.classList.remove('active'); // 👈 fecha modal
+    });
+  }
+});
   try {
     const resposta = await fetch(`/api/itens/${idItem}`);
     const item = await resposta.json();
